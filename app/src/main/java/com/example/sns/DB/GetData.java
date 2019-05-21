ca9fa92f15637641bbe2ce1882b7cba318c83c83
Black-Garlic <hn1226@naver.com>
2019-05-21 15:38:14 +0900

    데이터 불러오는거 수정

--- app/src/main/java/com/example/sns/DB/GetData.java
+++ app/src/main/java/com/example/sns/DB/GetData.java
@@ -11,6 +11,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class GetData {
     private String phpType[] = {
             "http://samuel1226.dothome.co.kr/project/user.php",
@@ -18,14 +20,10 @@ public class GetData {
             "http://samuel1226.dothome.co.kr/project/post.php",
             "http://samuel1226.dothome.co.kr/project/comment.php"
     };
-    StringBuffer sb = new StringBuffer();
 
-    public GetData() {
-    }
+    public GetData() {    }
 
-    public void result(User user){
-
-        Log.d("test", phpType[0]);
+    public void resultUser(ArrayList<User> userset){
 
         URLConnector task = new URLConnector(phpType[0]);
 
@@ -33,18 +31,17 @@ public class GetData {
 
         try {
             task.join();
-            Log.d("test", "try");
+            Log.d("test", "tryuser");
         } catch(InterruptedException e) {
             Log.d("test", ""+e);
         }
 
         String result = task.getResult();
 
-        Log.d("test", result);
-
         try {
             JSONArray jarray = new JSONArray(result);   // JSONArray 생성
             for(int i=0; i < jarray.length(); i++){
+                User user = new User();
                 JSONObject jObject = jarray.getJSONObject(i);  // JSONObject 추출
                 user.setIndex(jObject.getInt("index"));
                 user.setUser_name(jObject.getString("user_name"));
@@ -52,26 +49,15 @@ public class GetData {
                 user.setUser_pw(jObject.getString("user_pw"));
                 user.setUser_num(jObject.getInt("user_num"));
 
-                sb.append( //test를 위한 용도
-                        "\n" +
-                                "인덱스:" + user.getIndex() +
-                                "이름:" + user.getUser_name() +
-                                "아이디:" + user.getUser_id() +
-                                "비밀번호:" + user.getUser_pw() +
-                                "학번:" + user.getUser_num()
-                );
+                userset.add(user);
             }
-            Log.d("test", sb.toString()); //test를 위한 용도
-
         } catch (JSONException e) {
             Log.d("test", ""+e);
             e.printStackTrace();
         }
     }
 
-    public void result(Subject subject){
-
-        Log.d("test", phpType[1]);
+    public void resultSubject(ArrayList<Subject> subjectset){
 
         URLConnector task = new URLConnector(phpType[1]);
 
@@ -79,41 +65,31 @@ public class GetData {
 
         try {
             task.join();
-            Log.d("test", "try");
+            Log.d("test", "trysub");
         } catch(InterruptedException e) {
             Log.d("test", ""+e);
         }
 
         String result = task.getResult();
 
-        Log.d("test", result);
-
         try {
             JSONArray jarray = new JSONArray(result);   // JSONArray 생성
             for(int i=0; i < jarray.length(); i++){
+                Subject subject = new Subject();
                 JSONObject jObject = jarray.getJSONObject(i);  // JSONObject 추출
                 subject.setIndex(jObject.getInt("index"));
                 subject.setGrade(jObject.getString("grade"));
                 subject.setSubject_name(jObject.getString("subject_name"));
 
-
-                sb.append( //test를 위한 용도
-                        "\n" +
-                                "인덱스:" + subject.getIndex() +
-                                "학번:" + subject.getGrade() +
-                                "이름:" + subject.getSubject_name()
-                );
+                subjectset.add(subject);
             }
-            Log.d("test", sb.toString()); //test를 위한 용도
-
         } catch (JSONException e) {
             Log.d("test", ""+e);
             e.printStackTrace();
         }
     }
-    public void result(Post post){
 
-        Log.d("test", phpType[2]);
+    public void resultPost(ArrayList<Post> postset){
 
         URLConnector task = new URLConnector(phpType[2]);
 
@@ -128,11 +104,10 @@ public class GetData {
 
         String result = task.getResult();
 
-        Log.d("test", result);
-
         try {
             JSONArray jarray = new JSONArray(result);   // JSONArray 생성
             for(int i=0; i < jarray.length(); i++){
+                Post post = new Post();
                 JSONObject jObject = jarray.getJSONObject(i);  // JSONObject 추출
                 post.setIndex(jObject.getInt("index"));
                 post.setSubject_name(jObject.getString("subject_name"));
@@ -146,33 +121,17 @@ public class GetData {
                 post.setPoint_count(jObject.getInt("point_count"));
                 post.setView_count(jObject.getInt("view_count"));
 
-                sb.append( //test를 위한 용도
-                        "\n" +
-                                "인덱스:" + post.getIndex() +
-                                "과목:" + post.getSubject_name() +
-                                "아이디:" + post.getUser_id() +
-                                "제목:" + post.getTitle() +
-                                "내용:" + post.getContent() +
-                                "날짜:" + post.getEdit_date() +
-                                "like:" + post.getLike_count() +
-                                "점수:" + post.getPoint() +
-                                "점수합:" + post.getPoint_sum() +
-                                "점수갯수:" + post.getPoint_count() +
-                                "조회수:" + post.getView_count()
-                );
+                postset.add(post);
             }
-            Log.d("test", sb.toString()); //test를 위한 용도
-
         } catch (JSONException e) {
             Log.d("test", ""+e);
             e.printStackTrace();
         }
     }
-    public void result(Comment comment){
 
-        Log.d("test", phpType[0]);
+    public void resultComment(ArrayList<Comment> commentset){
 
-        URLConnector task = new URLConnector(phpType[0]);
+        URLConnector task = new URLConnector(phpType[3]);
 
         task.start();
 
@@ -185,11 +144,10 @@ public class GetData {
 
         String result = task.getResult();
 
-        Log.d("test", result);
-
         try {
             JSONArray jarray = new JSONArray(result);   // JSONArray 생성
             for(int i=0; i < jarray.length(); i++){
+                Comment comment = new Comment();
                 JSONObject jObject = jarray.getJSONObject(i);  // JSONObject 추출
                 comment.setIndex(jObject.getInt("index"));
                 comment.setPost_index(jObject.getInt("post_index"));
@@ -198,18 +156,8 @@ public class GetData {
                 comment.setEdit_date(jObject.getString("edit_date"));
                 comment.setLike_count(jObject.getInt("like_count"));
 
-                sb.append( //test를 위한 용도
-                        "\n" +
-                                "인덱스:" + comment.getIndex() +
-                                "과목인덱스:" + comment.getPost_index() +
-                                "아이디:" + comment.getUser_id() +
-                                "내용:" + comment.getContent() +
-                                "날짜:" + comment.getEdit_date() +
-                                "like:" + comment.getLike_count()
-                );
+                commentset.add(comment);
             }
-            Log.d("test", sb.toString()); //test를 위한 용도
-
         } catch (JSONException e) {
             Log.d("test", ""+e);
             e.printStackTrace();
